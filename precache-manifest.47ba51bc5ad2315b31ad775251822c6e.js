179dea3189a47cf6702da5774bd1b4db04a8f76f
straleboni <straleboni@gmail.com>
2020-10-18 11:00:34 +0200

    Updates

---
+++ precache-manifest.47ba51bc5ad2315b31ad775251822c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673f1ba6dc8f5c60b1edc9105ed3e524",
+    "url": "/straleboni/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e07537997041c0aff016",
+    "url": "/straleboni/monster-rolodex/static/css/main.acdb5f34.chunk.css"
+  },
+  {
+    "revision": "2d19631dba197ad7891e",
+    "url": "/straleboni/monster-rolodex/static/js/2.e1627e68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/straleboni/monster-rolodex/static/js/2.e1627e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07537997041c0aff016",
+    "url": "/straleboni/monster-rolodex/static/js/main.02e969a6.chunk.js"
+  },
+  {
+    "revision": "5cc5ca7eca03955cd4e2",
+    "url": "/straleboni/monster-rolodex/static/js/runtime-main.9a47c638.js"
+  }
+]);
